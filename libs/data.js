bd12ffc2a38169732a2e03386d0b06565b6624f3
MiguelLamas <lamasmigs@yahoo.co.uk>
2022-05-27 11:28:01 +0100

    import pg, pool, ssl, export query, animal data completed

---
+++ libs/data.js
@@ -0,0 +1,85 @@
+const animalsData = [
+    {		
+        id: 1,
+        animal_name: "bornean orangutan",
+        conservation_status: "critically endangered",
+        approx_population: 104700,
+        region: "se asia",
+        weight: "up to 200 pounds"
+    },
+
+    { 					
+        id: 2,
+        animal_name: "sumatran elephant",
+        conservation_status: "critically endangered",
+        approx_population: 2400,
+        region: "asia",
+        weight: "5 tons"
+    },
+    {				
+        id: 3,
+        animal_name: "sunda tiger",
+        conservation_status: "critically endangered",
+        approx_population: 400,
+        region: "africa",
+        weight: "165-308 pounds"
+    },
+    {			
+        id: 4,
+        animal_name: "blue whale",
+        conservation_status: "endangered",
+        approx_population: 15000,
+        region: "n & s america",
+        weight: "200 tons"
+    },
+    {	
+        id: 5,
+        animal_name: "green turtle",
+        conservation_status: "endangered",
+        approx_population: 90000,
+        region: "americas, africa",
+        weight: "150-400 pounds"
+    },
+    {				8-17 pounds
+        id: 6,
+        animal_name: "red panda",
+        conservation_status: "endangered",
+        approx_population: 10000,
+        region: "central asia",
+        weight: "8-17 pounds"
+    },
+    {				
+        id: 7,
+        animal_name: "great white shark",
+        conservation_status: "vulnerable",
+        approx_population: 3000,
+        region: "global oceans",
+        weight: "4000-7000 pounds"
+    },
+    {				
+        id: 8,
+        animal_name: "polar bear",
+        conservation_status: "vulnerable",
+        approx_population: 28000,
+        region: "arctic",
+        weight: "800–1,300 pounds (males), 300-700 pound (females)"
+    },
+    {				
+
+        id: 9,
+        animal_name: "jaguar",
+        conservation_status: "near threatened",
+        approx_population: 64000,
+        region: "south america",
+        weight: "120 to 300 pounds"
+    },
+    {				
+        id: 10,
+        animal_name: "tree kangaroo",
+        conservation_status: "least concern",
+        approx_population: 2500,
+        region: "australasia",
+        weight: "up to 16 pounds (males), 13 pounds (females)"
+    },
+
+]
